e64b80a94b9313365a8ccce3f21e6e5172a13cb9
vipink28 <vipinkalyan28@gmail.com>
2026-09-22 19:15:47 +0530

    Change API base URL to production endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://attendly-backend-zeqz.onrender.com/api",
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
 });
